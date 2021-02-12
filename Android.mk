64f6420e48c3ce5f9fc2d8da68fa25506a70d315
Jerzy Kołosowski <jerzy@kolosowscy.pl>
2021-02-12 08:33:02 +0100

    changed name because colission with microg FakeStore

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := privapp-permissions-com.android.vending.xml
+LOCAL_MODULE := privapp-permissions-phonesky.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
@@ -14,7 +14,7 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := default-permissions-com.android.vending.xml
+LOCAL_MODULE := default-permissions-phonesky.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/default-permissions
@@ -24,10 +24,10 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := Phonesky
-LOCAL_SRC_FILES := com.android.vending.apk
+LOCAL_SRC_FILES := Phonesky.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_REQUIRED_MODULES := privapp-permissions-com.android.vending.xml default-permissions-com.android.vending.xml
+LOCAL_REQUIRED_MODULES := privapp-permissions-phonesky.xml default-permissions-phonesky.xml
 include $(BUILD_PREBUILT)
